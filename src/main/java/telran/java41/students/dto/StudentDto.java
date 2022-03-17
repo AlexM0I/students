e735b5dc55bcf6ab0394d11a5abe13a34accd4eb
Alexandr Morozov <Alexandr Morozov@DESKTOP-F1BUPGU.mshome.net>
2022-03-18 00:43:33 +0200

    First commit

---
+++ src/main/java/telran/java41/students/dto/StudentDto.java
@@ -0,0 +1,19 @@
+package telran.java41.students.dto;
+
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+//@Setter
+@Builder
+public class StudentDto {
+	Integer id;
+	String name;
+	Map<String, Integer> scores;
+}
